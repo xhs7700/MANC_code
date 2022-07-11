7562c971fbe1f1ec63770d25393cd344aec5e12e
xhs7700 <xhs7700@126.com>
2022-07-12 00:41:49 +0800

    add function on OptimumSet

--- plot.py
+++ plot.py
@@ -3,55 +3,58 @@ import matplotlib.pyplot as plt
 import toml
 from os import path
 
-figsize = (10, 10)
+figsize = (8, 8)
+d = {0: '(a)', 1: '(b)', 2: '(c)', 3: '(d)'}
 
 
 def compare_effects(toml_name, input_dir, output_dir, algos):
     toml_path = path.join(input_dir, f'{toml_name}.toml')
     with open(toml_path, "r") as f:
         toml_obj = toml.load(f)
-    for graph_name, mancs in toml_obj.items():
-        fig, ax = plt.subplots(figsize=figsize, layout='tight')
+    fig, axs = plt.subplots(2, 2, figsize=figsize, layout='tight')
+    for i, (graph_name, mancs) in enumerate(toml_obj.items()):
+        ax = axs[(i >> 1) & 1, i & 1]
         n = len(mancs[algos[0]])
         x = np.arange(1, n + 1)
         for algo in algos:
             ax.plot(x, mancs[algo], label=algo)
         ax.set_xlabel(r'$k$')
         ax.set_ylabel(r'$M(S)$')
-        ax.set_title(graph_name)
+        ax.set_title(f'{d[i]} {graph_name}')
         ax.legend()
-        plt.savefig(path.join(output_dir, f'{toml_name}_{graph_name}.svg'))
-        plt.close(fig)
+    plt.savefig(path.join(output_dir, f'{toml_name}.eps'))
+    plt.close(fig)
 
 
 def margin_errors(toml_name, input_dir, output_dir, factors, label_prefix):
     toml_path = path.join(input_dir, f'{toml_name}.toml')
     with open(toml_path, "r") as f:
         toml_obj = toml.load(f)
+    fig, axs = plt.subplots(2, 2, figsize=figsize, layout='tight')
     x_pos = np.arange(6)
     xtick_label = [
         r'$(0,0.1]$', r'$(0.1,0.2]$', r'$(0.2,0.3]$', r'$(0.3,0.4]$',
         r'$(0.4,0.5]$', r'$(0.5,\infty]$'
     ]
     w = 0.2
-    for graph_name, errors in toml_obj.items():
-        fig, ax = plt.subplots(figsize=figsize, layout='tight')
+    for i, (graph_name, errors) in enumerate(toml_obj.items()):
+        ax = axs[(i >> 1) & 1, i & 1]
         for j, factor in enumerate(factors):
             ax.bar(x_pos + j * w,
                    height=errors[factor]['distribution'],
                    width=w,
                    label=f'{label_prefix} = {factor}')
-        ax.set_xlabel('error')
+        ax.set_xlabel(r'$error_u$')
         ax.set_ylabel('ratio')
-        ax.set_xticks(x_pos + 1.5 * w, xtick_label)
-        ax.set_title(graph_name)
+        ax.set_xticks(x_pos + 1.5 * w, xtick_label, fontsize='small')
+        ax.set_title(f'{d[i]} {graph_name}')
         ax.legend()
-        plt.savefig(path.join(output_dir, f'{toml_name}_{graph_name}.svg'))
-        plt.close(fig)
+    plt.savefig(path.join(output_dir, f'{toml_name}.eps'))
+    plt.close(fig)
 
 
-# compare_effects("compare_effects_exact", "outputs", "images",
-#                 ["rank", "exact", "degree", "absorb"])
+compare_effects("compare_effects_exact", "outputs", "images",
+                ["Top-SANC", "Exact", "Top-Degree", "Approx"])
 
 margin_errors("margin_errors", "outputs", "images", ["20", "50", "100", "200"],
               r"$c_{JL}$")
